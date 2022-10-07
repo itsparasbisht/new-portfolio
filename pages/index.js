833812dbcecdd51511f92dd35979376739b02ddb
itsparasbisht <iamparasbisht@gmail.com>
2022-10-07 20:24:45 +0530

    profile image

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,21 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import myProfileImage from "../assets/profile.jpg";
 
-console.log(myProfileImage);
-
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.box1}>
         <h1>Paras here</h1>
-        <h3>Javascript developer, since 2020</h3>
+        <h3>Javascript developer, since ...</h3>
       </div>
       <div className={styles.box2}>
-        <img
-          src={
-            "https://m.media-amazon.com/images/M/MV5BMTUxMzU2MTk1OF5BMl5BanBnXkFtZTgwNzg4NjAwMzI@._V1_.jpg"
-          }
-          alt=""
+        <Image
+          src={myProfileImage}
+          alt="This is Paras Bisht, the creator of this website."
+          width="500"
+          height="500"
+          className={styles.images}
+          placeholder="blur"
         />
       </div>
     </div>
