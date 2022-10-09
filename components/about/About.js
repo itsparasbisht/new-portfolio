ebf6e1c122997dd6ceb04b090cd7993e45714583
itsparasbisht <iamparasbisht@gmail.com>
2022-10-09 13:15:00 +0530

    about as object html and styles

--- components/about/About.js
+++ components/about/About.js
@@ -7,12 +7,38 @@ import jsLogo from "../../assets/Javascript-logo.png";
 function About() {
   return (
     <div className={styles.container}>
-      <div className={styles.box1}></div>
-      <div className={styles.box2}>
-        <div className={styles.logoCtr}>
-          <Image alt="react official logo" src={reactLogo} />
+      <div className={styles.box1}>
+        <div className={styles.aboutBox}>
+          <div className={styles.aboutHead}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <p>
+            <span>1</span> &nbsp;{" "}
+            <span className={styles.curlyBraces}>&#123;</span> <br />{" "}
+            <span>2</span> &nbsp; &nbsp; &nbsp; first_name:{" "}
+            <span className={styles.objString}>&#34;paras&#34;</span>, <br />{" "}
+            <span>3</span> &nbsp; &nbsp; &nbsp; last_name:{" "}
+            <span className={styles.objString}>&#34;bisht&#34;</span>, <br />{" "}
+            <span>4</span> &nbsp; &nbsp; &nbsp; gender:{" "}
+            <span className={styles.objString}>&#34;male&#34;</span>, <br />{" "}
+            <span>5</span> &nbsp; &nbsp; &nbsp; is_developer:{" "}
+            <span className={styles.objBoolean}>true</span>,
+            <br /> <span>6</span> &nbsp; &nbsp; &nbsp; description:{" "}
+            <span className={styles.objString}>
+              &#34;development is fun :) especially with JavaScript&#34; <br />{" "}
+            </span>{" "}
+            <span>7</span> &nbsp;{" "}
+            <span className={styles.curlyBraces}>&#125;</span>
+          </p>
         </div>
-        <div className={styles.logoCtr}>
+      </div>
+      <div className={styles.box2}>
+        {/* <div className={styles.logoCtr}>
+          <Image alt="react official logo" src={reactLogo} />
+        </div> */}
+        <div>
           <Image alt="javascript logo" src={jsLogo} />
         </div>
       </div>
