ee00fc67dd636fd6e6c631c23449e16548c096f3
itsparasbisht <iamparasbisht@gmail.com>
2022-10-09 12:20:13 +0530

    about section box2

--- components/about/About.js
+++ components/about/About.js
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "./about.module.css";
+import Image from "next/image";
+import reactLogo from "../../assets/React.webp";
+import jsLogo from "../../assets/Javascript-logo.png";
 
 function About() {
   return (
     <div className={styles.container}>
-      <h1>ABOUT</h1>
+      <div className={styles.box1}></div>
+      <div className={styles.box2}>
+        <div className={styles.logoCtr}>
+          <Image alt="react official logo" src={reactLogo} />
+        </div>
+        <div className={styles.logoCtr}>
+          <Image alt="javascript logo" src={jsLogo} />
+        </div>
+      </div>
     </div>
   );
 }
