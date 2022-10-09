1036281914ec38d5cf4a19029aa8c39aa075d299
itsparasbisht <iamparasbisht@gmail.com>
2022-10-09 14:05:07 +0530

    about position

--- components/about/About.js
+++ components/about/About.js
@@ -27,21 +27,29 @@ function About() {
             <span className={styles.objBoolean}>true</span>,
             <br /> <span>6</span> &nbsp; &nbsp; &nbsp; description:{" "}
             <span className={styles.objString}>
-              &#34;development is fun :) especially with JavaScript&#34; <br />{" "}
+              &#34;development is fun :) especially with JavaScript&#34;
+            </span>
+            ,
+            <span>
+              {" "}
+              <br /> 7
             </span>{" "}
-            <span>7</span> &nbsp;{" "}
+            &nbsp; &nbsp; &nbsp; skills:{" "}
+            <span className={styles.objString}>
+              <span className={styles.curlyBraces}>[</span>&#34;Javascript&#34;,
+              &#34;ES-6&#34;, &#34;ReactJs&#34;, &#34;NextJs&#34;,
+              &#34;RestfulAPI&#34;, <br /> &nbsp; &nbsp;
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+              &nbsp; &#34;MERN&#34;, &#34;Git&#34;, &#34;PostgreSQL&#34;
+              <span className={styles.curlyBraces}>]</span>
+            </span>
+            ,
+            <br /> <span>8</span> &nbsp;{" "}
             <span className={styles.curlyBraces}>&#125;</span>
           </p>
         </div>
       </div>
-      <div className={styles.box2}>
-        {/* <div className={styles.logoCtr}>
-          <Image alt="react official logo" src={reactLogo} />
-        </div> */}
-        <div>
-          <Image alt="javascript logo" src={jsLogo} />
-        </div>
-      </div>
+      <div className={styles.box2}></div>
     </div>
   );
 }
