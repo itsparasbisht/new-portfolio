f16c1c8f1c1d80879ef5623b45e778b8ee302097
itsparasbisht <iamparasbisht@gmail.com>
2022-10-15 20:21:45 +0530

    bored api call

--- components/bored/Bored.js
+++ components/bored/Bored.js
@@ -1,20 +1,42 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./bored.module.css";
-import feelingsImg from "../../assets/feelings.svg";
+import emojisImg from "../../assets/emojis.svg";
 
 function Bored() {
+  const [task, setTask] = useState("");
+
+  const getTask = () => {
+    try {
+      fetch("http://www.boredapi.com/api/activity/").then((response) =>
+        response.json().then((data) => {
+          setTask(data?.activity);
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h3>
-        Feeling bored! <br /> get some recommendations ;)
+        {task ? (
+          task
+        ) : (
+          <>
+            Feeling bored! <br /> get some recommendations ;)
+          </>
+        )}
       </h3>
-      <button>Suggest Something to do</button>
-      <Image
-        src={feelingsImg}
-        alt="If you are bored I can suggest you some things to do"
-        className={styles.images}
-      />
+      <button onClick={getTask}>Suggest Something to do</button>
+      <div className={styles.emojis}>
+        <Image
+          src={emojisImg}
+          alt="If you are bored I can suggest you some things to do"
+          className={styles.images}
+        />
+      </div>
     </div>
   );
 }
