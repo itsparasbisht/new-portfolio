33cc26badcd7b26efb3517a03e338f00a4426f1c
itsparasbisht <iamparasbisht@gmail.com>
2022-10-16 19:22:36 +0530

    kanye.rest call

--- components/kanyeQuotes/Quote.js
+++ components/kanyeQuotes/Quote.js
@@ -1,8 +1,40 @@
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import styles from "./quote.module.css";
+import kanyeImg from "../../assets/kanye.jpg";
 
 function Quote() {
-  return <div className={styles.container}>Quote</div>;
+  const [quote, setQuote] = useState();
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  function getQuote() {
+    try {
+      fetch("https://api.kanye.rest/").then((response) => {
+        response.json().then((data) => {
+          setQuote(data.quote);
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <Image
+        src={kanyeImg}
+        alt="Website Development Animated Illustration"
+        className={styles.images}
+        placeholder="blur"
+      />
+      <p>
+        {quote} <br /> <span>api.kanye.rest</span>{" "}
+      </p>
+    </div>
+  );
 }
 
 export default Quote;
