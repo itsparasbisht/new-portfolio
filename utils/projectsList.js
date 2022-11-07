1deddff7e51835a3eea449949538d78cfdfbae94
itsparasbisht <iamparasbisht@gmail.com>
2022-11-07 19:53:02 +0530

    project detail

--- utils/projectsList.js
+++ utils/projectsList.js
@@ -4,7 +4,8 @@ const projectsList = {
     technologies: "ReactJS, ExpressJS, MongoDB, Puppeteer, Nodemailer",
     imageUrl:
       "https://i.ibb.co/qDvN99R/Screenshot-2022-11-07-190009-Macbook.png",
-    about: "this is a project",
+    about:
+      "This is a project built using React and Express. Here you can paste the product url from Amazon to see the current price of an item. Then you can choose the price to get notified through mail whenever the price drops.",
     liveLink: "https://track-price-on-amazon.netlify.app/",
     githubLink: "https://github.com/itsparasbisht/price-tracker-backend",
   },
