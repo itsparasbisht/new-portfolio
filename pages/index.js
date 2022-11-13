fece35d490015287bbeef4789c7b094c493471bf
itsparasbisht <iamparasbisht@gmail.com>
2022-11-14 00:06:11 +0530

    import removed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import webDev from "../assets/web-dev.gif";
 import Navbar from "../components/navbar/Navbar";
 import About from "../components/about/About";
 import ShowCertificates from "../components/showCertificates/ShowCertificates";
