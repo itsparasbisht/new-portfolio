566b8818ac3952662f1079012378cbc180c73512
itsparasbisht <iamparasbisht@gmail.com>
2022-11-22 18:59:21 +0530

    head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Paras - Web Portfolio</title>
+      </Head>
+      <body>
+        <Component {...pageProps} />
+      </body>
+    </>
+  );
 }
 
 export default MyApp;
