36f57e0dd8227b046ea88970adaa0a9e86409efe
itsparasbisht <iamparasbisht@gmail.com>
2023-03-19 20:18:08 +0530

    image link

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,10 @@ export default function MyApp({ Component, pageProps }) {
           itemprop="description"
           content="I specialize in JavaScript development and have professional working experience as a frontend dev with ReactJS. I also have experience working with the MERN stack. I am a dedicated fellow who is interested in building solutions by coordinating with teams."
         />
-        <meta itemprop="image" content="https://ibb.co/FDD4DjX" />
+        <meta
+          itemprop="image"
+          content="https://i.ibb.co/3CCmCLv/paras-bisht.jpg"
+        />
 
         <meta property="og:url" content="https://paras-bisht.netlify.app" />
         <meta property="og:type" content="website" />
@@ -33,7 +36,10 @@ export default function MyApp({ Component, pageProps }) {
           property="og:description"
           content="I specialize in JavaScript development and have professional working experience as a frontend dev with ReactJS. I also have experience working with the MERN stack. I am a dedicated fellow who is interested in building solutions by coordinating with teams."
         />
-        <meta property="og:image" content="https://ibb.co/FDD4DjX" />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/3CCmCLv/paras-bisht.jpg"
+        />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Paras Bisht web portfolio" />
@@ -41,7 +47,10 @@ export default function MyApp({ Component, pageProps }) {
           name="twitter:description"
           content="I specialize in JavaScript development and have professional working experience as a frontend dev with ReactJS. I also have experience working with the MERN stack. I am a dedicated fellow who is interested in building solutions by coordinating with teams."
         />
-        <meta name="twitter:image" content="https://ibb.co/FDD4DjX" />
+        <meta
+          name="twitter:image"
+          content="https://i.ibb.co/3CCmCLv/paras-bisht.jpg"
+        />
       </Head>
       <Component {...pageProps} />
     </>
