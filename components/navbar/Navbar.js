5dbecee77fbcc4b44747639ae2dd1c540426cea2
itsparasbisht <iamparasbisht@gmail.com>
2024-06-03 20:09:54 +0530

    nav link fixes

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -6,40 +6,36 @@ function Navbar() {
   return (
     <div className={styles.container}>
       <ul>
-        <li className={styles.github_link}>
-          <a
-            href="https://github.com/itsparasbisht"
-            target="_blank"
-            rel="noreferrer"
-            onClick={() => {
-              posthog.capture("guest_opened_github");
-            }}
-          >
-            GitHub
-          </a>
-        </li>
-        <li className={styles.linkedin_link}>
-          <a
-            href="https://www.linkedin.com/in/paras-bisht"
-            target="_blank"
-            rel="noreferrer"
-            onClick={() => {
-              posthog.capture("guest_opened_linkedin");
-            }}
-          >
-            LinkedIn
-          </a>
-        </li>
-        <li className={styles.projects_link}>
-          <a
-            href="#projects-section"
-            onClick={() => {
-              posthog.capture("guest_clicked_projects");
-            }}
-          >
-            Projects
-          </a>
-        </li>
+        <a
+          href="https://github.com/itsparasbisht"
+          target="_blank"
+          rel="noreferrer"
+          onClick={() => {
+            posthog.capture("guest_opened_github");
+          }}
+        >
+          <li className={styles.github_link}>GitHub</li>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/paras-bisht"
+          target="_blank"
+          rel="noreferrer"
+          onClick={() => {
+            posthog.capture("guest_opened_linkedin");
+          }}
+        >
+          <li className={styles.linkedin_link}>LinkedIn</li>
+        </a>
+
+        <a
+          href="#projects-section"
+          onClick={() => {
+            posthog.capture("guest_clicked_projects");
+          }}
+        >
+          <li className={styles.projects_link}>Projects</li>
+        </a>
       </ul>
     </div>
   );
