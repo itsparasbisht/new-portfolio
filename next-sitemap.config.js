4053d1480ef789a2f4cf48b6db5c029adce77a77
itsparasbisht <iamparasbisht@gmail.com>
2024-10-02 14:13:19 +0530

    config changes

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://paras-bisht.netlify.app",
+  generateRobotsTxt: true, // Generate robots.txt
+  changefreq: "weekly",
+};
