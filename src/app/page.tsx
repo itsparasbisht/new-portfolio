dd9d0a763b0edc6e596cbe8fd43a5889416e43d6
itsparasbisht <iamparasbisht@gmail.com>
2025-08-27 13:08:28 +0530

    new version setup

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1
+        className="text-4xl mb-4"
+        style={{ fontFamily: "var(--font-basker)" }}
+      >
+        Hello from Baskervville
+      </h1>
+      <p style={{ fontFamily: "var(--font-lato)" }}>
+        This text should be in Lato
+      </p>
+    </main>
+  );
+}
